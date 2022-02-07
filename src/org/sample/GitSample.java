73d042972bc9e4b398f150e261a13c15a25447dc
ashaalavudeen <ashaparveenalavudeen@gmail.com>
2022-02-07 19:27:29 +0530

    task done

---
+++ src/org/sample/GitSample.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class GitSample {
+	
+ private void add() {
+	System.out.println("add");
+
+}
+ public static void main(String[] args) {
+	 GitSample g = new GitSample();
+	 g.add();
+}
+
+}
